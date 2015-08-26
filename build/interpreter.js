b57a34c471abed2ce59b132ed0c73e1590bdeb01
oxyflour <oxyflour@hotmail.com>
2015-08-26 14:56:22 +0800

    add build

---
+++ build/interpreter.js
@@ -0,0 +1,225 @@
+'use strict';
+
+var _slicedToArray = (function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i['return']) _i['return'](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError('Invalid attempt to destructure non-iterable instance'); } }; })();
+
+(function () {
+
+	var LAMBDA = 'lambda';
+	var LET = 'let';
+	var LETREC = 'letrec';
+	var SET = 'set';
+	var IF = 'if';
+	var COND = 'cond';
+
+	// [.let var1, val1, var2, val2, exp] =>
+	//   [[.lambda var1 var2 exp] val1 val2]
+	function evalLet(exp, env) {
+		var lambda = [LAMBDA],
+		    body = [lambda];
+		for (var i = 1; i < exp.length - 1; i += 2) {
+			lambda.push(exp[i]);
+			body.push(exp[i + 1]);
+		}
+		lambda.push(exp[exp.length - 1]);
+		return evaluate(body, env);
+	}
+
+	// [.letrec var1, val1, var2, val2, ... exp] =>
+	//   evaluate(exp, env_ext)
+	function evalLetrec(exp, env) {
+		env = environment(env);
+		for (var i = 1; i < exp.length - 1; i += 2) env(exp[i], evaluate(exp[i + 1], env));
+		return evaluate(exp[exp.length - 1], env);
+	}
+
+	// [.set var1 val1, var2, val2, ...]
+	function evalSet(exp, env) {
+		var pairs = [];
+		for (var i = 1; i < exp.length - 1; i += 2) pairs.push([exp[i], evaluate(exp[i + 1], env)]);
+		pairs.forEach(function (p) {
+			return env(p[0], p[1]);
+		});
+		return evaluate(exp[exp.length - 1], env);
+	}
+
+	// [.cond cond1, exp1, cond2, exp2, ... [condi], expi]
+	function evalCond(exp, env) {
+		for (var i = 1; i < exp.length - 1; i += 2) if (evaluate(exp[i], env)) return evaluate(exp[i + 1], env);
+		return evaluate(exp[exp.length - 1], env);
+	}
+
+	function environment(parent) {
+		var map = {};
+		return function (key, value) {
+			return value !== undefined ? map[key] = value : map[key] !== undefined ? map[key] : parent(key);
+		};
+	}
+
+	function closure(lambda, env) {
+		return { lambda: lambda, env: env };
+	}
+
+	function evaluate(exp, env) {
+		var head = exp[0];
+		if (head === LET) return evalLet(exp, env);else if (head === LETREC) return evalLetrec(exp, env);else if (head === SET) return evalSet(exp, env);else if (head === IF || head === COND) return evalCond(exp, env);
+
+		// core evaluation
+		else if (head === LAMBDA) return closure(exp, env);else if (Array.isArray(exp)) return apply(evaluate(exp[0], env), exp.slice(1).map(function (e) {
+				return evaluate(e, env);
+			}));else return env(exp);
+	}
+
+	function apply(func, args) {
+		// apply buildin function
+		if (func.apply) return func.apply(null, args);
+
+		// core apply
+		var lambda = func.lambda,
+		    env = environment(func.env);
+		args.forEach(function (a, i) {
+			return env(lambda[i + 1], a);
+		});
+		return evaluate(lambda[lambda.length - 1], env);
+	}
+
+	function rootenv() {
+		var _arguments = arguments;
+
+		var env = function env(key, value) {
+			if (value !== undefined) return map[key] = value;else if (typeof key !== 'string') return key;else if (key[0] === '"') return key.substr(1);else if (map[key] !== undefined) return map[key];else throw 'undefined variable `' + key + '`!';
+		};
+		var map = {
+
+			'nil': null,
+
+			'PI': Math.PI,
+
+			'^': function _(a, b) {
+				return Math.pow(a, b);
+			},
+
+			'*': function _(a, b) {
+				return a * b;
+			},
+			'/': function _(a, b) {
+				return a / b;
+			},
+
+			'+': function _(a, b) {
+				return a + b;
+			},
+			'-': function _(a, b) {
+				return a - b;
+			},
+
+			'>': function _(a, b) {
+				return a > b;
+			},
+			'<': function _(a, b) {
+				return a < b;
+			},
+			'=': function _(a, b) {
+				return a == b;
+			},
+			'>=': function _(a, b) {
+				return a >= b;
+			},
+			'<=': function _(a, b) {
+				return a <= b;
+			},
+			'==': function _(a, b) {
+				return a === b;
+			},
+
+			'begin': function begin() {
+				return _arguments[_arguments.length - 1];
+			},
+
+			'for': function _for(iterator, func) {
+				var data = [];
+				while (data = apply(iterator, data)) apply(func, data);
+			},
+
+			'range': function range() {
+				var start = 0,
+				    step = 1,
+				    end = 0,
+				    args = Array.prototype.slice.apply(_arguments);
+				if (args.length === 1) end = args[0];else if (args.length === 2) {
+					;
+
+					var _args = _slicedToArray(args, 2);
+
+					start = _args[0];
+					end = _args[1];
+				} else if (args.length === 3) {
+					;
+
+					var _args2 = _slicedToArray(args, 3);
+
+					start = _args2[0];
+					step = _args2[1];
+					end = _args2[2];
+				}return function (i) {
+					i = i === undefined ? start : i + step;
+					if (i >= start && i < end) return [i];
+				};
+			},
+
+			'pair': function pair(object) {
+				var keys = Object.keys(object);
+				return function (v, k, i) {
+					i = i === undefined ? 0 : i + 1;
+					if (i >= 0 && i < keys.length) {
+						k = keys[i];
+						return [object[k], k, i];
+					}
+				};
+			},
+
+			'print': function print() {
+				console.log.apply(console, _arguments);
+				return _arguments[_arguments.length - 1];
+			},
+
+			'.': function _(a, b, v) {
+				return v !== undefined ? (a[b] = v, a) : a[b];
+			},
+
+			'array': function array() {
+				return Array.prototype.slice.apply(_arguments);
+			},
+
+			'dict': function dict() {
+				var d = {};
+				for (var i = 0; i < _arguments.length; i += 2) {
+					var k = _arguments[i],
+					    v = _arguments[i + 1];
+					d[k === null ? i : k] = v;
+				}
+				return d;
+			}
+
+		};
+		/*
+   * some sample functions
+   *
+  'cons': (a, b) => [a, b],
+  'car': (a) => a[0],
+  'cdr': (a) => a[1],
+  'list': () => Array.prototype.slice.apply(arguments)
+  	.reduceRight((l, a) => [a, l], [ ]),
+  'map': (l, f) => l.length === 0 ? [ ] :
+  	[apply(f, [ l[0] ]), apply(map.map, [ l[1], f ])],
+  		'array': () => Array.prototype.slice.apply(arguments),
+  'array-map': (a, f) => a.map((v, i) => apply(f, [v, i])),
+  		'*2': closure([LAMBDA, 'x', ['*', 'x', 2]], env),
+  */
+		return env;
+	}
+
+	evaluate.rootenv = rootenv;
+	evaluate.environment = environment;
+
+	if (typeof module !== 'undefined') module.exports = evaluate;else if (typeof window !== 'undefined') window.evaluate = evaluate;
+})();
---
+++ src/index.js
@@ -0,0 +1,335 @@
+var line = 0, col = 1
+function count(value) {
+	if (value === '\n')
+		col = (line ++, 1)
+	else
+		col += value.length
+}
+
+function token(type, value, string) {
+	var t = { type, value, line, col }
+	count(string !== undefined ? string : value)
+	return t
+}
+
+function format(tree, depth) {
+	depth = depth || 0
+	var space = Array.apply(null, Array(depth)).map(v => '|  ').join('')
+	if (typeof(tree) !== typeof({ })) {
+		return tree + '\n'
+	}
+	else if (tree && 'type' in tree && 'value' in tree) {
+		return tree.value + ' [' + tree.type + ']' + '\n'
+	}
+	else if (depth < 10) {
+		var text = (tree.type ? '[' + tree.type + ']' : '' ) + '\n'
+		for (var key in tree) {
+			if (tree.hasOwnProperty(key) && key !== 'type')
+				text += space + key + ': ' + format(tree[key], depth + 1)
+		}
+		return text
+	}
+}
+
+function beginStringGen(tag) {
+	return function(m) {
+		this.push(tag);
+		this.tempString = ''
+		count(m)
+	}
+}
+function addString(m) {
+	this.tempString += m
+	count(m)
+}
+function escapeString(m) {
+	this.tempString += { n:'\n', t:'\t', r:'\r'}[ m[1] ] || m[1]
+	count(m)
+}
+function endString(m) {
+	this.pop();
+	return token('STR', this.tempString, m)
+}
+
+function beginCommentGen(tag) {
+	return function(m) {
+		this.push(tag)
+		count(m)
+	}
+}
+function eatComment(m) {
+	count(m)
+}
+function endComment(m) {
+	this.pop()
+	count(m)
+}
+function endCommentWithNewLine(m) {
+	this.pop()
+	return token('NEWLINE', ';', '\n')
+}
+
+var tokens = lex($('#input').text(), [
+
+	[/not/,
+		m => token('UNOP', m)],
+	[/\^/,
+		m => token('BINOP0', m)],
+	[/\*|\/|%/,
+		m => token('BINOP1', m)],
+	[/\+|-/,
+		m => token('BINOP2', m)],
+	[/>=|<=|==|>|<|~=/,
+		m => token('BINOP3', m)],
+	[/and|or|is|as/,
+		m => token('BINOP4', m)],
+
+	[/if|elseif|then|else|func|for|do|end|return/,
+		m => token(m, m)],
+
+	[/[a-zA-Z\$_]+\d*\w*/,
+		m => token('ID', m)],
+
+	// ref: https://github.com/moescript/moescript/blob/asoi/src/compiler/lexer.js
+	[/0[xX][a-fA-F0-9]+/,
+		m => token('NUM', parseInt(m), m)],
+	[/\d+(?:\.\d+(?:[eE]-?\d+)?)?/,
+		m => token('NUM', parseFloat(m), m)],
+
+	// strings
+	[/"/,			beginStringGen('string')],
+	[/\\[ntr"]/,	escapeString,	'string'],
+	[/[^\\"]*/,		addString,		'string'],
+	[/"/, 			endString,		'string'],
+	[/'/,			beginStringGen('string1')],
+	[/\\[ntr']/,	escapeString,	'string1'],
+	[/[^\\']*/,		addString,		'string1'],
+	[/'/, 			endString,		'string1'],
+
+	// comments
+	[/#/,			beginCommentGen('comment-sl')],
+	[/[^\n]+/,		eatComment,				'comment-sl'],
+	[/\n/,			endCommentWithNewLine,	'comment-sl'],
+	[/\[\[\[/,		beginCommentGen('comment-ml')],
+	[/[^(\]\]\])]/,	eatComment,				'comment-ml'],
+	[/\]\]\]/,		endComment,				'comment-ml'],
+
+	[/\[|{|\(|\]|}|\)|\.|=|,|\:/,
+		m => token(m, m)],
+
+	[/\n|;/,
+		m => token('NEWLINE', ';', m)],
+
+	[/[\t\r ]+/,
+		m => count(m)]
+])
+
+$('#tokens').text(tokens.map(t => t.value + ': ' + t.type).join('\n'))
+
+var tree = parse(tokens, [
+
+	['S', ['block']],
+
+	['block', ['statlist']],
+	['block', ['cstatlist']],
+
+	['cstatlist', ['statlist', 'laststat'],
+		(c, l) => c.concat(l)],
+	['cstatlist', ['cstatlist', 'NEWLINE']],
+
+	['statlist', ['NEWLINE'],
+		(n) => ['begin']],
+	['statlist', ['stat'],
+		(l, s) => ['begin', s]],
+	['statlist', ['statlist', 'stat'],
+		(l, s) => l.concat([s])],
+	['statlist', ['statlist', 'NEWLINE']],
+
+	['laststat', ['return'],
+		(_r) => null],
+	['laststat', ['return', 'explist'],
+		(_r, e) => e],
+
+	['stat', ['funcall']],
+	['stat', ['do', 'block', 'end'],
+		(_do, b, _end) => b],
+//	['stat', ['while', 'exp', 'do', 'block', 'end'],
+//		(_while, e, _do, b, _end) => ['while', ['lambda', i, b], e]],
+	['stat', ['repetition', 'do', 'block', 'end'],
+		(r, _do, b, _end) => ['for', r[1], ['lambda'].concat(r[0]).concat(b)]],
+	['stat', ['if', 'conds', 'end'],
+		(_if, c) => ['cond'].concat(c)],
+	['stat', ['func', 'funcname', 'funcbody'],
+		(_func, n, b) => ['set', n, b]],
+	['stat', ['setlist', '=', 'explist'], (a, _eq, e) => {
+		return a.reduce((l, i, j) => {
+			// transform [set [. obj key] val] -> [set obj [. obj key val]]
+			if (Array.isArray(i) && i[0] === '.') {
+				l.push(i[1])
+				l.push(['.', i[1], i[2], e[j]])
+			}
+			else {
+				l.push(i)
+				l.push(e[j])
+			}
+			return l
+		}, ['set'])
+	}],
+
+	['repetition', ['for', 'ID', '=', 'explist'],
+		(_for, i, _eq, e) => [[i], e.length === 1 ? e[0] : ['range'].concat(e)]],
+	['repetition', ['for', 'namelist', '=', 'explist'],
+		(_for, l, _eq, e) => [l, e.length === 1 ? e[0] : ['range'].concat(e)]],
+
+	['conds', ['condlist']],
+	['conds', ['condlist', 'else', 'block'],
+		(l, _else, b) => l.concat([1, b])],
+	['condlist', ['cond']],
+	['condlist', ['condlist', 'elseif', 'cond'],
+		(l, _elseif, c) => l.concat(c)],
+	['cond', ['exp', 'then', 'block'],
+		(e, _then, b) => [e, b]],
+
+	// binding is the same with set
+	['binding', ['local', 'namelist'],
+		(_var, a) => { throw 'not implemented' }],
+	['binding', ['local', 'namelist', '=', 'explist'],
+		(_var, a, _eq, e) => { throw 'not implemented' }],
+	['binding', ['local', 'func', 'ID', 'funcbody'],
+		(_var, _func, i, f) => { throw 'not implemented' }],
+
+	['funcname', ['dottedname']],
+	['funcname', ['dottedname', ':', 'ID'],
+		(p, _c, i, a) => { throw 'not implemented!' }],
+
+	['dottedname', ['ID']],
+	['dottedname', ['dottedname', '.', 'ID'],
+		(n, _c, i) => ['.', n, i]],
+
+	['namelist', ['ID'],
+		(i) => [i]],
+	['namelist', ['namelist', ',', 'ID'],
+		(a, _c, i) => a.concat([i])],
+
+	['explist', ['exp'],
+		(e) => [e]],
+	['explist', ['explist', ',', 'exp'],
+		(l, _c, e) => l.concat([e])],
+
+	['exp', ['NUM']],
+	['exp', ['STR']],
+	['exp', ['function']],
+	['exp', ['prefix']],
+	['exp', ['tableconst']],
+	['exp', ['UNOP', 'exp'],
+		(o, e) => [o, e]],
+	['exp', ['exp', 'BINOP4', 'exp'],
+		(e1, o, e2) => [o, e1, e2]],
+	['exp', ['exp', 'BINOP3', 'exp'],
+		(e1, o, e2) => [o, e1, e2]],
+	['exp', ['exp', 'BINOP2', 'exp'],
+		(e1, o, e2) => [o, e1, e2]],
+	['exp', ['exp', 'BINOP1', 'exp'],
+		(e1, o, e2) => [o, e1, e2]],
+	['exp', ['exp', 'BINOP0', 'exp'],
+		(e1, o, e2) => [o, e1, e2]],
+
+	['setlist', ['variable'],
+		(v) => [v]],
+	['setlist', ['setlist', ',', 'variable'],
+		(l, _c, v) => l.concat(v)],
+
+	['variable', ['ID']],
+	['variable', ['prefix', '[', 'exp', ']'],
+		(p, _l, e, _r) => ['.', p, e]],
+	['variable', ['prefix', '.', 'ID'],
+		(p, _dot, i) => ['.', p, token('STR', i.value, '')]],
+
+	['prefix', ['variable']],
+	['prefix', ['funcall']],
+	['prefix', ['(', 'exp', ')'],
+		(_l, e, _r) => e],
+
+	['funcall', ['prefix', 'args'],
+		(p, a) => [p].concat(a)],
+	['funcall', ['prefix', ':', 'ID', 'args'],
+		(p, _c, i, a) => { throw 'not implemented!' }],
+
+	['args', ['(', ')'],
+		(a) => [ ]],
+	['args', ['(', 'explist', ')'],
+		(_l, a, _r) => a],
+	['args', ['tableconst']],
+	['args', ['STR']],
+
+	['function', ['func', 'funcbody'],
+		(_f, b) => b],
+	['funcbody', ['params', 'block', 'end'],
+		(p, b) => ['lambda'].concat(p).concat([b])],
+
+	['params', ['(', ')'],
+		(p) => [ ]],
+	['params', ['(', 'parlist', ')'],
+		(_l, p, _r) => p],
+
+	['parlist', ['namelist']],
+	['parlist', ['...'],
+		(d) => { throw 'not implemented' }],
+	['parlist', ['namelist', '...'],
+		(d) => { throw 'not implemented' }],
+
+	['tableconst', ['{', '}'],
+		(t) => ['dict']],
+	['tableconst', ['{', 'fieldlist', '}'],
+		(_l, t, _r) => ['dict'].concat(t)],
+	['tableconst', ['{', 'fieldlist', ',', '}'],
+		(_l, t, _c, _r) => ['dict'].concat(t)],
+
+	['fieldlist', ['field']],
+	['fieldlist', ['fieldlist', ',', 'field'],
+		(l, _c, f) => l.concat(f)],
+
+	['field', ['exp'],
+		(e) => [null, e]],
+	['field', ['ID', '=', 'exp'],
+		(i, _eq, e) => [token('STR', i.value, ''), e]],
+	['field', ['[', 'exp', ']', '=', 'exp'],
+		(_l, e1, _r, _eq, e2) => [e1, e2]],
+
+], null, {
+	UNOP: [20, 'right'],
+	BINOP4: [10, 'left'],
+	BINOP3: [11, 'left'],
+	BINOP2: [12, 'left'],
+	BINOP1: [13, 'left'],
+	BINOP0: [14, 'right'],
+	'(': [1, 'right'],
+	'=': [1, 'right'],
+})
+
+var code = (function compile(tree) {
+	if (Array.isArray(tree))
+		return tree.map(compile)
+	else if (tree === undefined || tree === null)
+		return 'nil'
+	else if (tree.type === 'STR')
+		return '"' + tree.value
+	else if (tree.value !== undefined)
+		return tree.value
+	else
+		return tree
+})(tree)
+
+$('#tree').text(format(code))
+
+var env = evaluate.rootenv()
+// inject print function
+env('print', function() {
+	$('#result').append('<pre>' +
+		Array.prototype.slice.call(arguments).join(' ') +
+	'</pre>')
+})
+
+// go
+evaluate(code, env)
+
